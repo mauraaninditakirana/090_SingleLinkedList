270c82e2be2fa4ac967036e9b262b2af335cae83
mauraaninditakirana <kiranamaura351@gmail.com>
2024-04-26 13:46:54 +0700

    pembuatan prosedur add node

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -6,6 +6,17 @@ struct Node{
     string name;
     Node* next;
 };
+
+void addNode(){
+    int nim;
+    string nama;
+    Node* nodeBaru = new Node();
+    cout << "Masukkan NIM: ";
+    cin >> nim;
+    cout << "Masukkan nama: ";
+    cin >> nama;
+}
+
 Node* START = NULL;
 int main(){
     return 0;
