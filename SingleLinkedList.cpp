3497a80362a730c876126e1a26885d642263f656
mauraaninditakirana <kiranamaura351@gmail.com>
2024-04-26 14:24:37 +0700

    menerima input user fungsi yang akan dijalankan

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -114,5 +114,18 @@ void searchData(){
     }
 }
 int main(){
+    int pilihan;
+    do{
+        try{
+            cout << "1. Tambah Data" << endl;
+            cout << "2. Hapus Data" << endl;
+            cout << "3. Tampilkan Data" << endl;
+            cout << "4. Cari Data" << endl;
+            cout << "5. Keluar" << endl;
+            cin >> pilihan;
+        }catch(exception e){
+
+        }
+    }while(pilihan !=5);
     return 0;
 }
