6061e76ad734bc3585bc52902da1812e1a9f4f7b
mauraaninditakirana <kiranamaura351@gmail.com>
2024-04-26 14:04:38 +0700

    menambahkan prosedur penentuan ada tidaknya node dalam linked list

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -6,6 +6,7 @@ struct Node{
     string name;
     Node* next;
 };
+Node* START = NULL;
 
 void addNode(){
     int nim;
@@ -41,7 +42,22 @@ void addNode(){
     previous->next=nodeBaru;
 }
 
-Node* START = NULL;
+bool searchNode(int nim,Node* current,Node* previous){
+    previous = START;
+    current = START;
+    while(current != NULL && nim > current->noMhs){
+        previous = current;
+        current = current->next;
+    }
+    if(current == NULL){
+        return false;
+    }else if(current->noMhs==nim){
+        return true;
+    }else{
+        return false;
+    }
+
+}
 int main(){
     return 0;
 }
